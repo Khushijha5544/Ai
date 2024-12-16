051cb0a93973caf0bb5460642b5f1f6901d8e40c
Khushijha5544 <133341324+Khushijha5544@users.noreply.github.com>
2024-12-16 15:48:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,8 +2,8 @@ import os
 API_ID :int = int(os.environ.get("API_ID", None))
 API_HASH :str = os.environ.get("API_HASH", "")
 BOT_TOKEN :str= os.environ.get("BOT_TOKEN", "")
-UPDATE_CHNL :str = os.environ.get("UPDATE_CHNL", "mr_sukkun")
-SUPPORT_GRP :str = os.environ.get("SUPPORT_GRP", "the_support_chat")
+UPDATE_CHNL :str = os.environ.get("UPDATE_CHNL", "")
+SUPPORT_GRP :str = os.environ.get("SUPPORT_GRP", "")
 START_IMG :str = os.environ.get(
     "START_IMG", "https://graph.org/file/d4412c7b411ca8da9e177.jpg"
 )
@@ -12,5 +12,5 @@ MONGO_DB_URI :str = os.environ.get(
     "MONGO_DB_URI",
     "",
 )
-OWNER_ID :int= os.environ.get("OWNER_ID", 6728038801)
+OWNER_ID :int= os.environ.get("OWNER_ID", 5605603670)
 
